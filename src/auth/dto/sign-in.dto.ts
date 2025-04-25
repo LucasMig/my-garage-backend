88cf7045529b03a3e7e9c1ba9b87a4a9f40a0a80
LucasMig <lucasmigliori@gmail.com>
2025-04-25 13:41:33 -0300

    refactor: added ApiProperty decorators for Swagger

--- src/auth/dto/sign-in.dto.ts
+++ src/auth/dto/sign-in.dto.ts
@@ -1,9 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class SignInDto {
   @IsEmail()
+  @ApiProperty({
+    description: 'Email of the user',
+    example: 'john.doe@sample.com',
+  })
   email: string;
 
   @IsString()
+  @ApiProperty({ description: 'Password of the user', example: 'password123' })
   password: string;
 }
--- src/auth/dto/sign-up.dto.ts
+++ src/auth/dto/sign-up.dto.ts
@@ -1,12 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class SignUpDto {
   @IsString()
+  @ApiProperty({ description: 'Name of the user', example: 'John Doe' })
   name: string;
 
   @IsEmail()
+  @ApiProperty({
+    description: 'Email of the user',
+    example: 'john.doe@sample.com',
+  })
   email: string;
 
   @IsString()
+  @ApiProperty({ description: 'Password of the user', example: 'password123' })
   password: string;
 }
