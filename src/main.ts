144e9fdfb7a750698bb8a12b9ac402109634c44c
LucasMig <lucasmigliori@gmail.com>
2025-04-25 13:29:40 -0300

    feat: configure Swagger to ease API testing

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,40 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('MyGarage API')
+    .setDescription('API for MyGarage application')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api', app, documentFactory, {
+    swaggerOptions: {
+      docExpansion: 'none',
+      defaultModelsExpandDepth: -1,
+    },
+  });
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
     }),
   );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
